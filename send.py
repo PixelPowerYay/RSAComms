63d257fbea54b0dfee74256c8352402edc82cc95
PixelPowerYay <pixelpower.ro@gmail.com>
2017-10-27 13:52:06 +0300

    Minor bugfixes

--- send.py
+++ send.py
@@ -7,15 +7,17 @@ addr = str(input("Enter destination address: "))
 port = int(input("Enter port number: "))
 
 #### Key receiving
-srvsocket = socket.socket(AF_INET, SOCK_STREAM)
+srvsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 srvsocket.bind((addr, port))
 srvsocket.listen(1)
-(clientsocket, address) = serversocket.accept()
+(clientsocket, address) = srvsocket.accept()
 print("Connection established, transferring key...")
 time.sleep(1)
 key_pub_temp = clientsocket.recv(8129)
 key_pub = RSA.importKey(key_pub_temp)
 ###
+
+time.sleep(5)
 
 while True:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
