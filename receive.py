7dc71626c8cd41046efe9de792f2fcc8b2916cd1
PixelPowerYay <pixelpower.ro@gmail.com>
2017-10-27 13:44:21 +0300

    Total rewrite

--- receive.py
+++ receive.py
@@ -1,34 +1,29 @@
 from Crypto.PublicKey import RSA
+from Crypto import Random
 import socket
 import sys
 import time
 
+random_gen = Random.new().read
+key_private = RSA.generate(1024, random_gen)
+
 addr = str(input("Enter sender address: "))
 port = int(input("Enter port number: "))
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind((addr, port))
-msg_num = 0
-serversocket.listen(1)
-clientsocket, address = serversocket.accept()
-print("Connection from " + str(address))
+#### Key sending
+s = socket.socket(AF_INET, SOCK_STREAM)
+s.connect((addr, port))
+s.send(str(key_private.publicKey().exportKey()).encode())
+####
+
+
+srvsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+srvsocket.bind((addr, port))
+srvsocket.listen(1)
+(clientsocket, address) = srvsocket.accept()
+
+print("Main connection established with " + str(address))
 
 while True:
-	msg_num += 1
-	
-	if msg_num == 1:
-		key_pub_client = clientsocket.recv(65536).decode()
-		key_client_file = open('key', 'w')
-		key_client_file.write(str(key_pub_client))
-		key_client_file.close()
-		time.sleep(1)
-		key_file = open('key_internal', 'r')
-		key_private = RSA.importKey(key_file.read())
-		key_private.close()
-		
-	else:
-		data = key_private.decrypt(clientsocket.recv(8196).decode())
-		if not data:
-			break
-		print("> " + str(data))
-return 0
+	(clientsocket, address) = srvsocket.accept()
+	print("> " + str(key_private.decrypt(clientsocket.recv(8192).decode())))
