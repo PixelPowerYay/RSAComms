9a11eb4ea4dd87fcd3ba98c279e2dcccf21e05ab
PixelPowerYay <pixelpower.ro@gmail.com>
2017-10-30 14:43:48 +0000

    Fixed TypeError

--- receive.py
+++ receive.py
@@ -5,15 +5,18 @@ import sys
 import time
 
 random_gen = Random.new().read
-key_private = RSA.generate(1024, random_gen)
+key_private = RSA.generate(2048, random_gen)
 
 addr = str(input("Enter sender address: "))
 port = int(input("Enter port number: "))
 
+
 #### Key sending
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((addr, port+1))
-s.send(str(key_private.publickey().exportKey(format='PEM')).encode())
+key_str = str(key_private.publickey().exportKey(format='OpenSSH'))[:10]
+s.send(key_str.encode())
+#print(key_str)
 ####
 
 srvsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
