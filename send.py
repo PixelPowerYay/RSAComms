6325796eba768fb89b6f7b6c9ec08971fc9e7af8
PixelPowerYay <pixelpower.ro@gmail.com>
2017-11-01 17:32:53 +0200

    RSA instead of PyCrypto

--- send.py
+++ send.py
@@ -1,4 +1,3 @@
-from Crypto.PublicKey import RSA
 import socket
 import sys
 import time
@@ -8,12 +7,12 @@ port = int(input("Enter port number: "))
 
 #### Key receiving
 srvsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-srvsocket.bind((addr, port))
+srvsocket.bind((addr, port+1))
 srvsocket.listen(1)
 (clientsocket, address) = srvsocket.accept()
 print("Connection established, transferring key...")
 time.sleep(1)
-key_pub_temp = clientsocket.recv(8129)
+key_pub_temp = clientsocket.recv(8129).decode()
 key_pub = RSA.importKey(key_pub_temp)
 ###
 
